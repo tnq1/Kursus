cbac0ab561f7cc47a58f8c4b9d8b021c633e81f6
Tu Nga Quan (TNQ) <TNQ@kmd.dk>
2020-04-24 19:21:10 +0200

    Add project files.

---
+++ ALegeplads/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace ALegeplads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            Console.WriteLine("Hello World!");
+            Console.WriteLine();
+            var d = "";
+        }
+    }
+}
